2938b384d331227827daada61420a4dcf9c463c3
sakosakosa <sapotagekoki@gmail.com>
2026-07-27 23:41:58 +0900

    Initial Next.js project

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Welcome from "@/components/Welcome";
+
+export default function Home() {
+  return (
+    <main>
+      <Welcome name="AAAA" />
+      <Welcome name="BBBB" />
+      <Welcome name="CCCC" />
+    </main>
+  );
+}
